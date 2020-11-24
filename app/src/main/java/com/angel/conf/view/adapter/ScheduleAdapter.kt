042daba01e04c473ea894a811b622f64fa88cff2
Luis Angel <santosangel748@gmail.com>
2020-11-24 15:36:23 -0600

    Detalles del adaptador

--- app/src/main/java/com/angel/conf/view/adapter/ScheduleAdapter.kt
+++ app/src/main/java/com/angel/conf/view/adapter/ScheduleAdapter.kt
@@ -7,8 +7,11 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.angel.conf.R
 import com.angel.conf.model.Conferences
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ScheduleAdapter() : RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
+class ScheduleAdapter(val scheduleListener: ScheduleListener) : RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
 
     var listConference = ArrayList<Conferences>()
 
@@ -18,7 +21,28 @@ class ScheduleAdapter() : RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
     override fun getItemCount() = listConference.size
 
     override fun onBindViewHolder(holder: ScheduleAdapter.ViewHolder, position: Int) {
+        val conference = listConference[position] as Conferences
 
+        holder.tvConferenceName.text = conference.title
+        holder.tvConferenceSpeaker.text = conference.speaker
+        holder.tvConferenceTag.text = conference.tag
+
+        val simpleDataFormat = SimpleDateFormat("HH:mm")
+        val simpleDateFormatAMPM = SimpleDateFormat("a")
+
+        val cal = Calendar.getInstance()
+        cal.time = conference.datetime
+        val hourFormat = simpleDataFormat.format(conference.datetime)
+
+        holder.tvConferenceHour.text = hourFormat
+        holder.tvConferenceAMPM.text = simpleDateFormatAMPM.format(conference.datetime).toUpperCase()
+
+    }
+
+    fun updateData(data: List<Conferences>){
+        listConference.clear()
+        listConference.addAll(data)
+        notifyDataSetChanged()
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
---
+++ app/src/main/java/com/angel/conf/view/adapter/ScheduleListener.kt
@@ -0,0 +1,7 @@
+package com.angel.conf.view.adapter
+
+import android.telecom.Conference
+
+interface ScheduleListener {
+    fun onConferenceClicked(conference: Conference, position: Int)
+}
