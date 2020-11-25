16c157ee17dbc43426c49c2dd29a4e3a04fc7112
Luis Angel <santosangel748@gmail.com>
2020-11-25 16:43:36 -0600

    Creando el SplasScreen

--- app/src/main/java/com/angel/conf/view/ui/activities/SplashScreen.kt
+++ app/src/main/java/com/angel/conf/view/ui/activities/SplashScreen.kt
@@ -1,12 +1,38 @@
 package com.angel.conf.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.angel.conf.R
+import com.google.protobuf.Internal
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val animacion = AnimationUtils.loadAnimation(this, R.anim.animacion)
+        ivLogoPlatziConf.startAnimation(animacion)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        animacion.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+        })
     }
 }
