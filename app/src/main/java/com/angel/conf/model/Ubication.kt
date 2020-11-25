8db5551c02ad8a042caef10ca99de7584fac57de
Luis Angel <santosangel748@gmail.com>
2020-11-25 11:18:08 -0600

    Agregar y personalizar un marcador al mapa

--- app/src/main/java/com/angel/conf/model/Ubication.kt
+++ app/src/main/java/com/angel/conf/model/Ubication.kt
@@ -1,11 +1,11 @@
 package com.angel.conf.model
 
 class Ubication{
-    val name = ""
-    val address = ""
-    val latitude = ""
-    val longitude = ""
-    val phone = ""
-    val website = ""
-    val photo = ""
+    val name = "Centro Curtural Roberto Central"
+    val address = "Puente Xoco s/n, Xoco, 03330 Ciudad de Mexico, CDMX"
+    val latitude = 19.362028
+    val longitude = -99.166414
+    val phone = "+52 55 1519 6570"
+    val website = "https://elcantoral.com/"
+    val photo = "https://miro.medium.com/max/3840/1*rrRcaZKZsjerUveQqTSx7A.png"
 }
--- app/src/main/java/com/angel/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/angel/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,15 +1,23 @@
 package com.angel.conf.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.angel.conf.R
+import com.angel.conf.model.Ubication
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class UbicationFragment : Fragment(), OnMapReadyCallback {
 
@@ -21,12 +29,29 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
 
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitude, ubication.longitude)
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        val centerMark = LatLng(ubication.latitude, ubication.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+        markerOptions.title("PlatziConf 2019")
+
+        val bitmapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.logo_platzi) } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150, 150, false)
+
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+        googleMap?.addMarker(markerOptions)
     }
 
 }
